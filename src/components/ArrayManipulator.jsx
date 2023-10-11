42590243d876166ea83a213dc1d67de29b4656bb
JahongirPs <sancrops117@gmail.com>
2023-10-11 16:01:35 +0500

    first commit

--- src/components/ArrayManipulator.jsx
+++ src/components/ArrayManipulator.jsx
@@ -11,7 +11,7 @@ function ArrayManipulator() {
         Current Array: {array.join(', ')}
       </div>
       <button onClick={() => filter(num => num % 2 === 0)}>Filter</button>
-      <button onClick={() => push(6)}>Add </button>
+      <button onClick={() => push(1)}>Add </button>
       <button onClick={() => map(num => num * 2)}>Double</button>
       <button onClick={clear}>Clear</button>
     </div>
