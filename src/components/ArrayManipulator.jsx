c33f54c1f945c8a70bb82609d3b9cd93c7e63ce2
JahongirPs <sancrops117@gmail.com>
2023-10-11 15:58:46 +0500

    first commit

---
+++ src/components/ArrayManipulator.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import useFetch from '../hooks/useFetch';
+
+
+function ArrayManipulator() {
+  const { array, filter, map, push, removeByIndex, clear } = useFetch([1, 2, 3, 4, 5]);
+
+  return (
+    <div>
+      <div className='ArrayBox'>
+        Current Array: {array.join(', ')}
+      </div>
+      <button onClick={() => filter(num => num % 2 === 0)}>Filter</button>
+      <button onClick={() => push(6)}>Add </button>
+      <button onClick={() => map(num => num * 2)}>Double</button>
+      <button onClick={clear}>Clear</button>
+    </div>
+  );
+}
+
+export default ArrayManipulator;
